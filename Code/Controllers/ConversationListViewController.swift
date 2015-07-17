a6a56624bf736c744b0493748db00feaf308205d
kwkhaw <kwkhaw@gmail.com>
2015-07-17 08:32:37 +0800

    Bug Fix for issue #2: Prevent the user from accessing the conversation before the participants' name are resolved.

--- Code/Controllers/ConversationListViewController.swift
+++ Code/Controllers/ConversationListViewController.swift
@@ -27,10 +27,13 @@ class ConversationListViewController: ATLConversationListViewController, ATLConv
     // MARK - ATLConversationListViewControllerDelegate Methods
 
     func conversationListViewController(conversationListViewController: ATLConversationListViewController, didSelectConversation conversation:LYRConversation) {
-        let controller = ConversationViewController(layerClient: self.layerClient)
-        controller.conversation = conversation
-        controller.displaysAddressBar = true
-        self.navigationController!.pushViewController(controller, animated: true)
+        let unresolvedParticipants: NSArray = UserManager.sharedManager.unCachedUserIDsFromParticipants(Array(conversation.participants))
+        if unresolvedParticipants.count == 0 {
+            let controller = ConversationViewController(layerClient: self.layerClient)
+            controller.conversation = conversation
+            controller.displaysAddressBar = true
+            self.navigationController!.pushViewController(controller, animated: true)
+        }
     }
 
     func conversationListViewController(conversationListViewController: ATLConversationListViewController, didDeleteConversation conversation: LYRConversation, deletionMode: LYRDeletionMode) {
